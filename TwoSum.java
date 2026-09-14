8172122600f94cfb3eefb5b77161fd0b2d633692
Fazekas Bálint <fazekasbalint003@gmail.com>
2026-09-14 20:26:56 +0200

    TwoSum

---
+++ TwoSum.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map <Integer, Integer> map = new HashMap<>();  
+
+        for (int i = 0; i< nums.length; i++){
+            int curr = nums[i];
+            int need = target - curr;
+
+            if(map.containsKey(need)){
+                return new int[]{
+                    map.get(need),
+                    i
+                };
+            }
+            map.put(curr, i);
+        }      
+        return new int[] {};
+    }
+}
